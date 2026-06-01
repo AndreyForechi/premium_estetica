1a418f0f83f25fc23428125206dd8c91d64d946e
Andrey Rosa Forechi <andreyrf@weg.net>
2026-06-01 19:56:24 -0300

    Mudança na logo

--- src/App.jsx
+++ src/App.jsx
@@ -25,11 +25,13 @@ export function App() {
       {/* Navbar */}
       <header className="fixed top-0 w-full bg-black/60 backdrop-blur-xl border-b border-white/10 z-50">
         <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
-          <a href="#home"><img
-            src="/logo.jpg"
-            alt="Logo"
-            className="w-13 rounded-full"
-          /></a>
+          <a href="#home">
+            <div
+              
+              alt="Logo"
+              className="w-13 h-13 rounded-full bg-white text-black text-sm flex items-center justify-center font-bold">LOGO</div>
+            <div />
+          </a>
 
           <nav className="hidden md:flex gap-8 text-sm text-zinc-300">
             <a href="#about" className="hover:text-white transition">
